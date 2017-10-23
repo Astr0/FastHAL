402ac977e1eb9c312a6ff91ef1d7e38c6e1303c3
Andriy Obrizan <aastr00@gmail.com>
2017-10-23 15:48:42 +0300

    playground project

---
+++ examples/Playground/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Playground.cpp
+ *
+ * Created: 10/23/2017 3:48:05 PM
+ * Author : astr0
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
