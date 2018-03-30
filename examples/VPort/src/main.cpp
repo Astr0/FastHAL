3963af15c14e2952e64ef22a5182dad5b79a52d8
astr0 <aastr00@gmail.com>
2018-03-30 21:03:42 +0300

    ifdef for old impl

--- examples/VPort/src/main.cpp
+++ examples/VPort/src/main.cpp
@@ -1,3 +1,4 @@
+//#define VFIELD_OLD
 #include <avr/io.h>
 #include "fasthal.h"
 
@@ -5,7 +6,7 @@ using namespace fasthal;
 
 //constexpr auto testPort = vField(pinB4, pinB5, pinB6, pinD0, invert(pinD2), nullBit);
 // using TestPort = VField<PinB4, PinB5, PinB6, PinD0, PinD2Inv, NullPin>;
-constexpr auto testPort = vField(pinB0, pinB1);
+constexpr auto testPort = vField(pinB1, pinB2);
 
 //constexpr auto testPin = fieldBit<0>(portB);
 constexpr auto testPin = portB0;
