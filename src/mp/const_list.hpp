eb9d9857a821168fa07e5e8f7ab047609f6bc466
Andriy Obrizan <aastr00@gmail.com>
2018-04-02 00:19:43 +0300

    more statics for const_list

--- src/mp/const_list.hpp
+++ src/mp/const_list.hpp
@@ -2,6 +2,7 @@
 #define FH_CONSTLIST_H_
 
 #include "brigand.hpp"
+#include "../std/type_traits.hpp"
 #include "../std/std_fake.hpp"
 
 namespace fasthal{
@@ -9,8 +10,17 @@ namespace fasthal{
         namespace details{
             template<class TElement>
             struct is_static_element_impl: brigand::false_type{ };
+
+            template<typename T, T V>
+            struct is_static_element_impl<std::integral_constant<T, V>>: std::true_type{};
+
+            template<typename T, T V>
+            struct is_static_element_impl<brigand::integral_constant<T, V>>: std::true_type{};    
+
+            template<class T>
+            using is_static_element = is_static_element_impl<std::base_type_t<T>>;
                
-            template<class TElement, std::size_t Index = 0, bool is_static = is_static_element_impl<std::base_type_t<TElement>>::value>
+            template<class TElement, std::size_t Index = 0, bool is_static = is_static_element<TElement>::value>
             struct element_holder{
                 const TElement element;
                 
@@ -21,6 +31,8 @@ namespace fasthal{
 
             template<class TElement, std::size_t Index>
             struct element_holder<TElement, Index, true>{
+                constexpr element_holder() { }
+
                 constexpr element_holder(TElement __element) { }
 
                 constexpr TElement getElement(){return TElement{};}
@@ -34,6 +46,9 @@ namespace fasthal{
         struct const_list<T, TRest...>: 
             details::element_holder<T, (sizeof...(TRest))>, 
             const_list<TRest...>{
+
+            //template<std::enable_if_c<details::is_static_element<const_list<T, TRest...>>::value> dummy = nullptr>
+            constexpr const_list(){}
 
             constexpr const_list(const T __el, const TRest... __rest): details::element_holder<T, (sizeof...(TRest))>{__el}, const_list<TRest...>{__rest...}
             {                
@@ -51,7 +66,10 @@ namespace fasthal{
                     return list.holder_t::getElement();
                 }
             };
-            
+
+            // const list statis if all elements are static
+            template<typename... T>
+            struct is_static_element_impl<const_list<T...>>: brigand::all<const_list<T...>, brigand::bind<is_static_element, brigand::_1>> { };
         }
 
         template<class... TElements>
