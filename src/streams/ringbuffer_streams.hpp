72994a163d95414bf823cf18508e6819759f59ca
astr0 <aastr00@gmail.com>
2018-04-13 10:41:13 +0300

    uart and transmitters have static interface insted of external

--- src/streams/ringbuffer_streams.hpp
+++ src/streams/ringbuffer_streams.hpp
@@ -6,13 +6,20 @@
 namespace fasthal{
     template<class TTarget, unsigned VCapacity>
     struct ring_buffer_transmitter{
-        static constexpr auto async = true;
-        static constexpr auto target = TTarget{};
+        static constexpr auto async = true;        
         static ring_buffer<VCapacity> buffer;
+
+        // transmitter
+        static inline auto next(){
+            auto c = buffer.read_dirty();
+            auto last = buffer.empty();
+            return has_byte{ c, last };
+        }
     };
 
-    template<class TTarget, unsigned VCapacity>
-    ring_buffer<VCapacity> ring_buffer_transmitter<TTarget, VCapacity>::buffer{};
+    // buggy gcc AVR generates wierd intialization for this
+    // template<class TTarget, unsigned VCapacity>
+    // ring_buffer<VCapacity> ring_buffer_transmitter<TTarget, VCapacity>::buffer{};
 
     namespace details{
         template<class T, unsigned C>
@@ -23,29 +30,22 @@ namespace fasthal{
     template<class TTarget, unsigned C>
     inline void write(ring_buffer_transmitter<TTarget, C> trans, std::uint8_t c){
         // shortcut if empty buffer -> try write something sync
-        if (trans.buffer.empty() && try_write(trans.target, c))           
+        if (trans.buffer.empty() && TTarget::try_write(c))           
             return;
 
         // wait for buffer space
         while (!trans.buffer.try_write(c))
-            try_write_sync(trans.target);
+            TTarget::try_write_sync();
         
         // start writing
-        write_async(trans.target);
+        TTarget::write_async();
     }
 
-    // transmitter
-    template<class TTarget, unsigned C>
-    inline auto next(ring_buffer_transmitter<TTarget, C> trans){
-        auto c = trans.buffer.read_dirty();
-        auto last = trans.buffer.empty();
-        return has_byte{ c, last };
-    }
 
     // commit on transmitter - ensures that everything that was written was send to target, not really written by target!
     template<class TTarget, unsigned C>
     inline void flush(ring_buffer_transmitter<TTarget, C> trans){
-        flush(trans.target);
+        TTarget::flush();
     }
 }
 #endif
