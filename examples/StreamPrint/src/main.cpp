478f43d4d687dbfbdc0f1af04747615747cde764
Andriy Obrizan <aastr00@gmail.com>
2018-04-14 13:40:20 +0300

    print example, flush example

--- examples/StreamPrint/src/main.cpp
+++ examples/StreamPrint/src/main.cpp
@@ -1,9 +1,3 @@
-//#define UART1
-//#include <Arduino.h>
-
-//#define RX_SIZE 64
-//#define TX_SIZE 64
-
 #include "fasthal.hpp"
 
 //#define _FLASH(x) FH_FLASH(x)
@@ -11,20 +5,6 @@
 
 using namespace fasthal;
 using namespace fasthal::avr;
-
-#ifdef TX_SIZE
-static constexpr auto uart0 = uart<0, ring_buffer_transmitter<TX_SIZE>::type>{};
-FH_UART_TX(0, uart0);
-#ifdef UART1
-static constexpr auto uart1 = uart<1, ring_buffer_transmitter<TX_SIZE>::type>{};
-FH_UART_TX(1, uart1);
-#endif
-#else
-static constexpr auto uart0 = uart<0>{};
-#ifdef UART1
-static constexpr auto uart1 = uart<1>{};
-#endif
-#endif
 
 template<typename T>
 void testPrint(T writer, uint8_t read){
@@ -42,18 +22,12 @@ int main(){
 	apply(
         enable(irq),
         uart0.begin()
-        #ifdef UART1
-        , uart1.begin(baud_v<115000>)
-        #endif
     );    
 
     std::uint8_t i = 0;
 	while (true){
         auto read = i++;
         
-        testPrint(uart0.tx, read);
-        #ifdef UART1
-        testPrint(uart1.tx, read);
-        #endif
+        testPrint(uart_sync_tx<0>{}, read);
 	}
 }
