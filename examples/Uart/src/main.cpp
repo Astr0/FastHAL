6067e8ce5ffb649f31066aa0c839e4246edd440d
Andriy Obrizan <aastr00@gmail.com>
2018-04-14 13:29:45 +0300

    nice uart example

--- examples/Uart/src/main.cpp
+++ examples/Uart/src/main.cpp
@@ -1,15 +1,59 @@
+#define MODE 6
+// 1 - echo sync
+// 2 - a...z sync
+// 3 - echo async
+// 4 - a...z asyc
+// 5 - echo arduino
+// 6 - a..z arduino
+
+
 #include "fasthal.hpp"
-//#include <Arduino.h>
+
+#if (MODE == 5 || MODE == 6)
+#include <Arduino.h>
+#endif
 
 using namespace fasthal;
 
-// void uart0_rx(){
-//     auto ok = uart0.rx_ok();
-//     auto v = uart0.rx();
-//     if (ok)
-//         tx_sync(uart0, v);
-// }
+int main(){
+    enable_(irq);
+    #if (MODE == 5 || MODE == 6)
+    Serial.begin(9600);
+    #elif
+    uart0.begin_(baud_v<9600>);
+    #endif
 
+    #if (MODE == 4)
+    uart0.tx_start();
+    #endif
+
+    while (true){
+        #if (MODE == 1)
+        try_rx(uart0, [](auto c){ tx_sync(uart0, c); });
+        #elif (MODE == 2)
+        for(auto x = 'a'; x <= 'z'; ++x)
+            tx_sync(uart0, x);
+        tx_sync(uart0, 10);
+        tx_sync(uart0, 13);
+        #elif (MODE == 5)
+        Serial.write(Serial.read());
+        #elif (MODE==6)
+        for(auto x = 'a'; x <= 'z'; ++x)
+             Serial.write(x);
+         Serial.println();        
+        #endif
+    }
+}
+
+#if (MODE == 3)
+void uart0_rx(){
+    auto ok = uart0.rx_ok();
+    auto v = uart0.rx();
+    if (ok)
+        tx_sync(uart0, v);
+}
+FH_UART_RX(0, uart0_rx);
+#elif (MODE == 4)
 char c = 'a';
 void uart0_tx(){
     switch(c){
@@ -26,28 +70,5 @@ void uart0_tx(){
     }
     c++;
 }
-
-int main(){
-    enable_(irq);
-    uart0.begin_(baud_v<9600>);
-    uart0.tx_start();
-    while (true){
-        // try_rx(uart0, [](auto c){ tx_sync(uart0, c); });
-
-        // for(auto x = 'a'; x <= 'z'; ++x)
-        //     tx_sync(uart0, x);
-        // tx_sync(uart0, 10);
-        // tx_sync(uart0, 13);
-        // tx_done_wait(uart0);
-    }
-
-    // Serial.begin(9600);
-    // while (true){
-    //     for(auto x = 'a'; x <= 'z'; ++x)
-    //         Serial.write(x);
-    //     Serial.println();
-    // }
-}
-
-//FH_UART_RX(0, uart0_rx);
 FH_UART_TX(0, uart0_tx);
+#endif
