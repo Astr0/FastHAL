4dd7f8e51b489227b4decab0bfe09b12c201d896
Andriy Obrizan <aastr00@gmail.com>
2018-04-16 01:25:05 +0300

    some kernel cleanup

--- src/sys/kernel.hpp
+++ src/sys/kernel.hpp
@@ -18,73 +18,64 @@ namespace fasthal{
     template<std::size_t VMaxTasks, class TTimer = details::sys_kernel_timer<>>
     struct sys_kernel{
         using timer_t = TTimer;
-        using due_t = decltype(timer_t::now());
+        using time_t = decltype(timer_t::now());
         //static constexpr auto debug = uart_sync_tx<0>{};
 
         class task_time{            
-            due_t _due;
+            time_t _due;
             task_t _task;
         public:
             task_time(){}
-            task_time(due_t due, task_t task): _due(due), _task(task) {}
+            task_time(time_t due, task_t task): _due(due), _task(task) {}
 
             bool for_task(task_t task) { return _task == task; }
 
-            due_t due_elapsed(due_t time) { return _due - time; }
+            time_t timeout(time_t time) { return _due - time; }
             task_t task() { return _task; }
         };
   
 
         static auto now() { return timer_t::now(); }
 
-        list<task_time, VMaxTasks> _tasks;
         volatile bool _changed;
+        list<task_time, VMaxTasks> _tasks;
 
         void tasks_changed() { _changed = true; }
 
-        due_t next_due_elapsed(due_t last){
-            while (true){
-                // wait for tasks
-                while (_tasks.empty()){                    
-                }
+        time_t next_timeout(time_t last){
+            time_t result = ~time_t{};
 
-                {                    
-                    auto lock = no_irq{};
-                    // someone removed the only task =/
-                    auto sz = _tasks.size();
-                    if (sz == 0)
-                        continue;
-                    auto result = _tasks.at(--sz).due_elapsed(last);
-                    while (sz--)
-                    {
-                        auto due = _tasks.at(sz).due_elapsed(last);
-                        if (due < result)
-                            result = due;
-                    }
-                    return result;                    
-                }
+            auto lock = no_irq{};
+            auto sz = _tasks.size();
+            while (sz--)
+            {
+                auto to = _tasks.at(sz).timeout(last);
+                if (to < result)
+                    result = to;
             }
+            // this will return max timeout for empty tasks!, but we'll wait for change anyway
+            return result;
         }        
 
-        due_t wait_next_due_or_change(due_t last){
+        time_t wait_next_timeout_or_change(time_t last){
             // time to next task from start
-            auto next_due = next_due_elapsed(last);
+            auto wait = next_timeout(last);
             //print(debug, "next:");
             //println(debug, next_due);
 
             // wait for time
-            due_t elapsed;
-            while (((elapsed = now() - last) < next_due) && !_changed);
+            time_t elapsed;
+            while (((elapsed = now() - last) < wait) && !_changed);
 
             return elapsed;
         }
 
-        task_t pop_due_task(due_t last, due_t elapsed){
+        task_t pop_time_task(time_t last, time_t elapsed){
             auto lock = no_irq{};
 
             auto i = _tasks.size();
             while (i--){
-                if (_tasks.at(i).due_elapsed(last) <= elapsed)
+                if (_tasks.at(i).timeout(last) <= elapsed)
                 {
                     auto task = _tasks.at(i).task();
                     _tasks.remove_at(i);
@@ -94,15 +85,12 @@ namespace fasthal{
             return nullptr;
         }
     public:
-        bool setTimeout(due_t timeout, task_t task){
+        bool setTimeout(time_t timeout, task_t task){
+            timeout += timer_t::now();
             auto lock = no_irq{};
-            if (_tasks.full()){
-                //println(debug, "nadd");
-                
+            if (_tasks.full())
                 return false;
-            }
-            auto due = timer_t::now() + timeout;
-            _tasks.add(task_time{ due, task });
+            _tasks.add(task_time{ timeout, task });
             tasks_changed();
             //println(debug, "add");
             return true;
@@ -121,16 +109,16 @@ namespace fasthal{
         }
 
         void run(){
-            due_t last = 0;                        
+            time_t last = 0;                        
             while (true){
                 // reset changed since we'll find next due
                 _changed = false;
-                auto elapsed = wait_next_due_or_change(last);
+                auto elapsed = wait_next_timeout_or_change(last);
                 // restart if changed?                
                 // ok, probably elapsed more than time to next due, lets do some tasks!
                 // add if (_changed) continue; to speed up things if changed
                 task_t task;
-                while ((task = pop_due_task(last, elapsed)) != nullptr){
+                while ((task = pop_time_task(last, elapsed)) != nullptr){
                     //println(debug, "exec");
                     task();
                 }
