beecf18e5ed7c359be692256ca44e34fb19ce55c
Andriy Obrizan <aastr00@gmail.com>
2018-04-15 14:25:26 +0300

    settimeout example

---
+++ examples/SetTimeout/src/main.cpp
@@ -0,0 +1,12 @@
+#include "fastduino.hpp"
+
+//constexpr auto kernel = kernel<10>{};
+
+int main(){
+    // apply(
+        
+    // );
+
+    // kernel.setTimeout(0, blinkLed);
+    // kernel.run();
+}
