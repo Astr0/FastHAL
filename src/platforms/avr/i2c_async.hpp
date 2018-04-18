b870277a4672dfec9df4dcaaf08b463529d62bb6
astr0 <aastr00@gmail.com>
2018-04-18 17:27:08 +0300

    670 on fsm

--- src/platforms/avr/i2c_async.hpp
+++ src/platforms/avr/i2c_async.hpp
@@ -18,6 +18,79 @@ namespace fasthal{
         volatile std::uint8_t* _buf;
         volatile bsize_t _count;
         TCallback _callback;
+
+        bool fsm(i2c_async_r& res){
+            auto c = _count;
+            auto v = _buf;
+            //i2c_async_r res;
+            // return here - exit
+            // break == call irq
+            switch (_i2c.state().state()){
+                case i2c_s::mt: // select_w sent, received ACK. Need write or start/stop/stop_start
+                case i2c_s::mt_write: // MT write, received ACK. Need write or start/stop/stop_start
+                    // done, goto irq
+                    if (c == 0) {
+                        res = i2c_async_r::done;
+                        break;
+                    }
+                tx:       
+                    _count = c - 1;
+                    _buf = v + 1;
+                txo:
+                    _i2c.tx(*v);
+                    return false;
+                case i2c_s::m_start: // Entered START. Need select_w or select_r
+                case i2c_s::m_restart: // Entered repeated START. Need select_w or select_r
+                    // SLA+W, just normal TX it
+                    if (((*v) & 1) == 0)
+                        goto tx;
+                    // count bytes will be overriden in place of SLA
+                    goto txo;
+                case i2c_s::mt_nack: // select_w sent, received NACK. Need write or start/stop/stop_start
+                case i2c_s::mt_write_nack: // MT write, received NACK. Need write or start/stop/stop_start
+                case i2c_s::mr_nack: // select_r sent, received NACK. Need read, readlast, repeated start, stop, stop_start
+                    // call IRQ
+                    res = i2c_async_r::nack;
+                    break;
+                case i2c_s::bus_fail:  // HW error on bus (invalid START/STOP condition). Need for bus restart.                    
+                case i2c_s::m_la: // another master took of the bus unexpectedly in select_w, select_r or write/readl. Need fail or start.
+                    // call IRQ
+                    res = i2c_async_r::error;
+                    break;
+                case i2c_s::mr_read: // recevied byte ok. Need read/readl
+                case i2c_s::mr_readl: // nack sent to slave after receiving byte, stop restart or stop/start will be transmitted, mr                    
+                    *v = _i2c.rx();
+                    _buf = v + 1;
+                case i2c_s::mr: // select_r sent, received ACK. Need read/readl or start/stop/stop_start
+                    if (c == 0) {
+                        res = i2c_async_r::done;
+                        break;
+                    }
+                    _i2c.rx_ask(--c);
+                    _count = c;
+                    return false;
+                // slave statuses
+                case i2c_s::sr: // received own sla-w, ACK returned, will receive bytes and ACK/NACK, sr
+                case i2c_s::sr_la: // arbitration lost in master sla-r/w, slave address matched
+                case i2c_s::sr_cast: // broadcast has been received, ACK returned, will receive bytes and ACK/NACK, sr
+                case i2c_s::sr_cast_la: // arbitration lost in master sla-r/w, sla+w broadcast, will receive bytes and ACK/NACK, sr
+                case i2c_s::sr_read: // own data has been received, ACK returned, will receive bytes and ACK/NACK, sr
+                case i2c_s::sr_readl: // own data has been received, NACK returned, reseting TWI, sr
+                case i2c_s::sr_read_cast: // broadcast data has been received, ACK returned, will receive bytes and ACK/NACK, sr
+                case i2c_s::sr_readl_cast: // broadcast data has been received, NACK returned, reseting TWI, sr
+                case i2c_s::sr_stop_restart: // stop or start has been received while still addressed, reseting TWI, sr
+                case i2c_s::st: // received own sla-r, ACK returned, will send bytes, st
+                case i2c_s::st_la: // arbitration lost in master sla-r/w, slave address matched
+                case i2c_s::st_write: // data byte was transmitted and ACK has been received, will send bytes, st
+                case i2c_s::st_writel: // data byte was transmitted and NACK has been received, reseting TWI, st
+                case i2c_s::st_writel_ack: // last data byte was transmitted and ACK has been received, reseting TWI, st
+                // we shouldn't get ISR for ready state, so don't handle it
+                case i2c_s::ready: // no errors, ok state?
+                default:
+                    return false;
+            }
+            return true;
+        }
     public:
         // first byte in buffer should be SLA
         bool start(
@@ -65,77 +138,11 @@ namespace fasthal{
 
         // ISR - just do what it takes
         void operator()(){
-            auto c = _count;
-            auto v = _buf;
             i2c_async_r res;
-            // return here - exit
-            // break == call irq
-            switch (_i2c.state().state()){
-                case i2c_s::mt: // select_w sent, received ACK. Need write or start/stop/stop_start
-                case i2c_s::mt_write: // MT write, received ACK. Need write or start/stop/stop_start
-                    // done, goto irq
-                    if (c == 0) {
-                        res = i2c_async_r::done;
-                        break;
-                    }
-                tx:       
-                    _count = c - 1;
-                    _buf = v + 1;
-                txo:
-                    _i2c.tx(*v);
-                    return;
-                case i2c_s::m_start: // Entered START. Need select_w or select_r
-                case i2c_s::m_restart: // Entered repeated START. Need select_w or select_r
-                    // SLA+W, just normal TX it
-                    if (((*v) & 1) == 0)
-                        goto tx;
-                    // count bytes will be overriden in place of SLA
-                    goto txo;
-                case i2c_s::mt_nack: // select_w sent, received NACK. Need write or start/stop/stop_start
-                case i2c_s::mt_write_nack: // MT write, received NACK. Need write or start/stop/stop_start
-                case i2c_s::mr_nack: // select_r sent, received NACK. Need read, readlast, repeated start, stop, stop_start
-                    // call IRQ
-                    res = i2c_async_r::nack;
-                    break;
-                case i2c_s::bus_fail:  // HW error on bus (invalid START/STOP condition). Need for bus restart.                    
-                case i2c_s::m_la: // another master took of the bus unexpectedly in select_w, select_r or write/readl. Need fail or start.
-                    // call IRQ
-                    res = i2c_async_r::error;
-                    break;
-                case i2c_s::mr_read: // recevied byte ok. Need read/readl
-                case i2c_s::mr_readl: // nack sent to slave after receiving byte, stop restart or stop/start will be transmitted, mr                    
-                    *v = _i2c.rx();
-                    _buf = v + 1;
-                case i2c_s::mr: // select_r sent, received ACK. Need read/readl or start/stop/stop_start
-                    if (c == 0) {
-                        res = i2c_async_r::done;
-                        break;
-                    }
-                    _i2c.rx_ask(--c);
-                    _count = c;
-                    return;
-                // slave statuses
-                case i2c_s::sr: // received own sla-w, ACK returned, will receive bytes and ACK/NACK, sr
-                case i2c_s::sr_la: // arbitration lost in master sla-r/w, slave address matched
-                case i2c_s::sr_cast: // broadcast has been received, ACK returned, will receive bytes and ACK/NACK, sr
-                case i2c_s::sr_cast_la: // arbitration lost in master sla-r/w, sla+w broadcast, will receive bytes and ACK/NACK, sr
-                case i2c_s::sr_read: // own data has been received, ACK returned, will receive bytes and ACK/NACK, sr
-                case i2c_s::sr_readl: // own data has been received, NACK returned, reseting TWI, sr
-                case i2c_s::sr_read_cast: // broadcast data has been received, ACK returned, will receive bytes and ACK/NACK, sr
-                case i2c_s::sr_readl_cast: // broadcast data has been received, NACK returned, reseting TWI, sr
-                case i2c_s::sr_stop_restart: // stop or start has been received while still addressed, reseting TWI, sr
-                case i2c_s::st: // received own sla-r, ACK returned, will send bytes, st
-                case i2c_s::st_la: // arbitration lost in master sla-r/w, slave address matched
-                case i2c_s::st_write: // data byte was transmitted and ACK has been received, will send bytes, st
-                case i2c_s::st_writel: // data byte was transmitted and NACK has been received, reseting TWI, st
-                case i2c_s::st_writel_ack: // last data byte was transmitted and ACK has been received, reseting TWI, st
-                // we shouldn't get ISR for ready state, so don't handle it
-                case i2c_s::ready: // no errors, ok state?
-                default:
-                    return;
+            if (fsm(res)){
+                _count = 0;
+                _callback(res);
             }
-            _count = 0;
-            _callback(res);
         }
     };
 }
