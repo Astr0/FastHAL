a11e6dbe6eddf7aee52303b87486888bc9f4a8fe
astr0 <aastr00@gmail.com>
2018-04-27 19:30:44 +0300

    gateway or node

--- examples/MySensors/src/main.cpp
+++ examples/MySensors/src/main.cpp
@@ -1,3 +1,5 @@
+#define MY_GATEWAY
+
 #include "fastduino.hpp"
 #include "libs/mysensors.hpp"
 
@@ -6,11 +8,13 @@ using namespace fasthal::duino;
 using namespace fasthal::dev;
 using namespace fasthal::mysensors;
 
-//static constexpr auto uart0 = uart<0>{};
-//static auto uart0tx = uart_buf_tx<uart<0>, 32>{};
-//static auto uart0rx = uart_buf_rx<uart<0>, 32>{};
-//FH_UART_TX(0, uart0tx);
-//FH_UART_RX(0, uart0rx);
+#ifdef MY_GATEWAY
+static constexpr auto uart0 = uart<0>{};
+static auto uart0tx = uart_buf_tx<uart<0>, 32>{};
+static auto uart0rx = uart_buf_rx<uart<0>, 32>{};
+FH_UART_TX(0, uart0tx);
+FH_UART_RX(0, uart0rx);
+#endif
 
 static constexpr auto spi0 = spi<0>{};
 static constexpr auto spi0h = spi_sync<spi<0>>{};
@@ -27,19 +31,13 @@ static constexpr auto ntransport = ntransport_rf24{ FH_SPTR(context), FH_SPTR(ra
 // direct transport
 static constexpr auto transport = direct_transport{ FH_SPTR(context), FH_SPTR(ntransport) };
 // and UART gateway transport
-//static auto gtransport = gtransport_streams{ FH_SPTR(uart0tx), FH_SPTR(uart0rx) };
+#ifdef MY_GATEWAY
+static auto gtransport = gtransport_streams{ FH_SPTR(uart0tx), FH_SPTR(uart0rx) };
 // and gateway with transport and gateway transport
-//static constexpr auto gateway = mygateway{ FH_SPTR(transport), FH_SPTR(gtransport) };
+static constexpr auto gateway = mygateway{ FH_SPTR(transport), FH_SPTR(gtransport) };
+#else
 static constexpr auto node = mynode{ FH_SPTR(transport) };
-
-void debug_radio(){
-    // print(uart0tx, "config: ");
-    // print(uart0tx, radio.config());
-    // print(uart0tx, "channel: ");
-    // print(uart0tx, radio.channel());
-    // print(uart0tx, " rf: ");
-    // println(uart0tx, radio.rf());
-}
+#endif
 
 int main(){        
     apply(        
@@ -54,23 +52,34 @@ int main(){
         , makeOutput(ino<SCK>)
         , makeOutput(ino<MOSI>)
         , spi0.begin(spi_clock_v<2'000'000UL>)
+        #ifdef MY_GATEWAY
         //init uart
-        //, uart0.begin()
+        , uart0.begin()
+        #endif
         , enable(irq)
     );
 
-    if (!node.begin()) {
+    #ifdef MY_GATEWAY
+    auto ok = gateway.begin();
+    #else
+    auto ok = node.begin();
+    #endif
+
+    if (!ok) {
         // can't turn led - it's on SCK pin =/
-        //print(uart0tx, "node error. ");
+        print(uart0tx, "error. ");
         //debug_radio();
         while (1);
     }
 
     while (1){
         auto msg = mymessage { };
+        #ifdef MY_GATEWAY
+        if (gateway.update(msg)){
+            print(uart0tx, "got message");
+        }
+        #else
         node.update(msg);
-        // if (gateway.update(msg)){
-        //     print(uart0tx, "got message");
-        // }
+        #endif
     }
 }
